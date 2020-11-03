f88debca0a7807e3dda940a0f7699816b71ae838
Tobdu399 <topias.niiranen12@gmail.com>
2020-11-03 21:14:21 +0200

    Initial Release

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ pygame.init()
 path = pathlib.Path(__file__).resolve().parent
 font = str(path) + "/lib/font.ttf"
 
-shapes_list = [shapes.S, shapes.Z, shapes.I, shapes.O, shapes.J, shapes.T]
-shapes_to_draw = []
-
 grid = 20
 black = pygame.Color(0, 0, 0)
 grey = pygame.Color(50, 50, 50)
@@ -24,33 +21,43 @@ clock = pygame.time.Clock()
 display = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tetris")
 
+shapes_list = [shapes.S, shapes.Z, shapes.I, shapes.O, shapes.J, shapes.T]
+colors_list = [pygame.Color(3, 65, 174), pygame.Color(114, 203, 59), pygame.Color(255, 213, 0), pygame.Color(255, 151, 28), pygame.Color(255, 50, 19)]
+locked_shapes = []
+
 class Piece:
-    def __init__(self, shape):
+    def __init__(self, shape, color):
         self.x = grid
         self.y = 0
         self.shape = shape
         self.rotation = 0
-        self.locked_position = []
-        self.locked = False
+        self.color = color
+        self.collision = False
     
     def show(self):
         x = 0
         y = 0
-
         for row in self.shape[self.rotation % len(self.shape)]:
             for col in row:
-                if col == "0" and self.y+y >= HEIGHT-(6*grid):
-                    self.locked_position.extend((self.x, self.y))
-                    self.locked = True
-                
-                x_pos = self.x if len(self.locked_position) == 0 else self.locked_position[0]
-                y_pos = self.y if len(self.locked_position) == 0 else self.locked_position[1]
-
+                if col == "0" and self.y+y == HEIGHT-(6*grid):
+                    self.collision = True
                 if col == "0":
-                    pygame.draw.rect(display, blue, (x_pos+x, y_pos+y, grid, grid))
+                    pygame.draw.rect(display, self.color, (self.x+x, self.y+y, grid, grid))
                 x += grid
             x = 0
             y += grid
+            
+        for _ in locked_shapes:
+            lckd_x = 0
+            lckd_y = 0
+            locked_shape = _.shape[_.rotation % len(_.shape)]
+            for lckd_row in locked_shape:
+                for lckd_col in lckd_row:
+                    if lckd_col == "0":
+                        pygame.draw.rect(display, _.color, (_.x+lckd_x, _.y+lckd_y, grid, grid))
+                    lckd_x += grid
+                lckd_x = 0
+                lckd_y += grid
 
 def move_piece():
     while not GAMEOVER:
@@ -79,19 +86,16 @@ def show_fps(surface, font_size, color, xy):
 
 threading.Thread(target = move_piece).start()
 
-piece = Piece(random.choice(shapes_list))
-shapes_to_draw.append(piece)
+piece = Piece(random.choice(shapes_list), random.choice(colors_list))
 
 while not GAMEOVER:
     display.fill(black)
 
-    for shape in shapes_to_draw:
-        shape.show()
+    piece.show()
 
-    if all_equal(shape.locked for shape in shapes_to_draw):
-        if len(shapes_to_draw) == 1 and shapes_to_draw[0].locked or len(shapes_to_draw) > 1:
-            piece = Piece(random.choice(shapes_list))
-            shapes_to_draw.append(piece)
+    if piece.collision:
+        locked_shapes.append(piece)
+        piece = Piece(random.choice(shapes_list), random.choice(colors_list))
 
     draw_grid(display, grid, grey)
     draw_borders(display, light_grey)
@@ -106,20 +110,17 @@ while not GAMEOVER:
         if event.type == pygame.KEYDOWN:
             #! grid = one block
             
-            for shape in shapes_to_draw:
-                if shape.locked == False:
-                    # Rotate
-                    if event.key == pygame.K_UP:
-                        if len(piece.locked_position) == 0:
-                            shape.rotation += 1
-                    
-                    # Move
-                    if event.key == pygame.K_RIGHT:
-                        shape.x += grid
-                    if event.key == pygame.K_LEFT:
-                        shape.x -= grid
-                    if event.key == pygame.K_DOWN:
-                        shape.y += grid
+            # Rotate
+            if event.key == pygame.K_UP:
+                piece.rotation += 1
+            
+            # Move
+            if event.key == pygame.K_RIGHT:
+                piece.x += grid
+            if event.key == pygame.K_LEFT:
+                piece.x -= grid
+            if event.key == pygame.K_DOWN:
+                piece.y += grid
 
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(90)
